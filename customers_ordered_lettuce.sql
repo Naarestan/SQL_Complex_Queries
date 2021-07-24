5bcc7415890f8b9d7f468781896089ba46182db0
Nastaran Mahmoodzadeh <63141665+Naarestan@users.noreply.github.com>
2021-07-24 15:20:33 -0700

    customers_ordered_lettuce.sql

---
+++ customers_ordered_lettuce.sql
@@ -0,0 +1,21 @@
+-- Find customers who have ordered Lettuce (id = 3)
+-- Select Customer ID, First Name, and Last Name
+
+USE sql_store;
+
+SELECT
+	customer_id,
+    first_name,
+    last_name
+FROM
+	customers
+WHERE
+	customer_id IN
+		(
+        SELECT customer_id
+		FROM orders
+		WHERE order_id IN
+			(SELECT order_id
+			FROM order_items
+			WHERE product_id = 3)
+		)
