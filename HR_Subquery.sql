e9f9a90174a0fb5fd7c5cc7ad919f1d755c7d97e
Nastaran Mahmoodzadeh <63141665+Naarestan@users.noreply.github.com>
2021-07-24 12:59:45 -0700

    HR_Subquery.sql

---
+++ HR_Subquery.sql
@@ -0,0 +1,14 @@
+-- In SQL_HR Database:
+-- Find employees who earn more than average
+-- And, work in the office # 3
+
+USE sql_hr;
+
+SELECT *
+FROM employees
+WHERE salary > (
+	SELECT AVG(salary)
+    FROM employees
+)
+AND
+office_id = 3;
