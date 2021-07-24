773b93fdaf15a15339e19cc89f9b683ff864f208
Nastaran Mahmoodzadeh <63141665+Naarestan@users.noreply.github.com>
2021-07-24 12:52:51 -0700

    Subquery in WHERE Clause
    -- Find products that are more expensive than Lettuce
    -- Lettuce (id = 3)

---
+++ Subquery.sql
@@ -0,0 +1,10 @@
+-- Find products that are more expensive than Lettuce
+-- Lettuce (id = 3)
+
+SELECT *
+FROM products
+WHERE unit_price > (
+	SELECT unit_price
+    FROM products
+    WHERE product_id = 3
+)
