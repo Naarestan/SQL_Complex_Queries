b0b736720568b93c4a247e74d392cdc693c4b509
Nastaran Mahmoodzadeh <63141665+Naarestan@users.noreply.github.com>
2021-07-24 21:33:51 -0700

    Correlated_Subqueries.sql

---
+++ Correlated_Subqueries.sql
@@ -0,0 +1,13 @@
+-- for each employee
+-- calculate the average salary for employee.office
+-- return the employee if the salary > average
+
+USE sql_hr;
+
+SELECT *
+FROM employees AS e
+WHERE salary > (
+	SELECT AVG(salary)
+    FROM employees
+    WHERE office_id = e.office_id
+)
