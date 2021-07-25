b3d6ae36904bbe64a3cfd12e10c9fb5c6f290b4d
Nastaran Mahmoodzadeh <63141665+Naarestan@users.noreply.github.com>
2021-07-24 18:04:38 -0700

    Update customers_ordered_lettuce.sql

--- customers_ordered_lettuce.sql
+++ customers_ordered_lettuce.sql
@@ -20,23 +20,36 @@ WHERE
 			(SELECT order_id
 			FROM order_items
 			WHERE product_id = 3)
-	)
+	);
 
 
 
 --- Solution # 2
 
-SELECT *
+SELECT customer_id,
+	first_name,
+	last_name
 FROM customers
 WHERE customer_id IN (
 	SELECT o.customer_id
 	FROM order_items AS oi
 	JOIN orders AS o USING (order_id)
-	WHERE product_id =3
-)
+	WHERE product_id = 3
+);
 
 
 --- Solution # 3
+
+SELECT
+	DISTINCT customer_id,
+	first_name,
+	last_name
+FROM customers AS c
+JOIN orders AS o USING (customer_id)
+JOIN order_items AS oi USING (order_id)
+WHERE oi.product_id = 3;
+
+---
 
 
 
