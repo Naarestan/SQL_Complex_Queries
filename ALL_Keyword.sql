d847f524b480b0c73d7c544fabf7511c5792bd79
Nastaran Mahmoodzadeh <63141665+Naarestan@users.noreply.github.com>
2021-07-24 18:21:40 -0700

    ALL_Keyword
    Select invoices that are larger than all invoices of client 3

---
+++ ALL_Keyword.sql
@@ -0,0 +1,32 @@
+-- Selec invoices that are larger than all invoices of client 3
+
+-- Solution # 1
+
+USE sql_invoicing;
+
+SELECT *
+FROM invoices
+WHERE invoice_total > (
+	SELECT MAX(invoice_total)
+    FROM invoices
+    WHERE client_id = 3
+);
+
+
+-- Solution # 2
+
+SELECT *
+FROM invoices
+WHERE invoice_total > ALL (
+	SELECT invoice_total
+    FROM invoices
+    WHERE client_id = 3
+);
+
+
+-------
+-- Whereever we have a ALL keyword, we can rewrite that query using a MAX aggregate function
+-------
+
+
+
