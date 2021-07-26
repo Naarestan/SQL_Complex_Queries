630abe7909ddf4a5fe5262c190480ce79508c587
Nastaran Mahmoodzadeh <63141665+Naarestan@users.noreply.github.com>
2021-07-25 22:50:24 -0700

    Update SELECT_Subquery.sql

--- SELECT_Subquery.sql
+++ SELECT_Subquery.sql
@@ -6,10 +6,10 @@
 SELECT
 	DISTINCT client_id,
     	name,
-		(SELECT SUM(invoice_total)
-		 FROM invoices
-		 WHERE client_id = c.client_id) AS total_sales,
-		(SELECT AVG(invoice_total)
+	(SELECT SUM(invoice_total)
+	 	FROM invoices
+	 	WHERE client_id = c.client_id) AS total_sales,
+	(SELECT AVG(invoice_total)
 		FROM invoices) AS average,
     	(SELECT total_sales - average) AS difference
 FROM clients AS c;
