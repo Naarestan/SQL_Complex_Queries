74928840b00c5ef0212f632c3e0ac85a20bf0c67
Nastaran Mahmoodzadeh <63141665+Naarestan@users.noreply.github.com>
2021-07-25 22:20:25 -0700

    Update EXISTS_operator.sql

--- EXISTS_operator.sql
+++ EXISTS_operator.sql
@@ -28,3 +28,28 @@ WHERE EXISTS (
 -- EXISTS Operator is the more efficient way because the more data we have, the longer time SQL needs to read through WHERE IN clause for each data in the list.
 -- When we use EXISTS Operator, the subquery does not return a result to the outer query. 
 -- Instead, it will return an indication of whether any rows in the subquery matches this search condition: WHERE client_id = c.client_id
+
+
+
+
+-- Find the products that have never been ordered
+
+USE sql_store;
+
+-- Solution # 1
+SELECT *
+FROM products AS p
+WHERE product_id NOT IN (
+	SELECT product_id
+    FROM order_items
+);
+
+
+-- Solution # 2
+SELECT *
+FROM products AS p
+WHERE NOT EXISTS (
+	SELECT product_id
+    FROM order_items
+    WHERE product_id = p.product_id
+);
